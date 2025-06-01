ee7fba67152ead7dec1400609894f5f922acabf0
indranilmondal901 <122368872+indranilmondal901@users.noreply.github.com>
2025-06-01 11:28:53 +0530

    Port issue -fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = 5000;
 console.log(port)
 const { Server } = require('socket.io');
 const Actions = require('./src/Actions');
